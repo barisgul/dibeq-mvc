8c75bb34df20c3b3196996fb61faca814588ff11
Barış Gül <bariss.gull@gmail.com>
2020-03-25 23:33:29 +0300

    CustomerForm added

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -20,19 +20,53 @@ namespace dibeq_mvc.Controllers
             _context.Dispose();
         }
 
-        public ActionResult New()
+        public ActionResult CustomerForm() 
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipTypes
             };
-            return View(viewModel);
+            return View("CustomerForm",viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer) 
+        {
+            if (customer == null)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+            }            
+            _context.SaveChanges(); //db'ye kaydeder
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer.Equals(null))
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm", viewModel);
         }
 
         public ViewResult Index()
         {
-            var customers = _context.Customers.Include(c =>c.MembershipType).ToList();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
 
---
+++ ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿using dibeq_mvc.Models;
+using System.Collections.Generic;
+
+namespace dibeq_mvc.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
